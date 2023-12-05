c384e3169ff679341c4236d9eb46e6f387562edd
kirkya99 <fabianschotte@outlook.com>
2023-12-05 20:29:47 +0100

    Tutorial Buttons

--- app/src/main/java/de/rwu/ws2021_vl1/MainActivity.java
+++ app/src/main/java/de/rwu/ws2021_vl1/MainActivity.java
@@ -46,12 +46,18 @@ public class MainActivity extends AppCompatActivity {
                 .setAction("Action", null).show();
     }
 
+    public void anotherButtonClicked(View view) {
+        Snackbar.make(view, "another Button", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
